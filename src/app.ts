2e61463a244800e1cac5e99def43b1263e5dc0c9
Rafael Freire <rafaeldacostafreire@gmail.com>
2022-05-01 19:10:40 -0300

    First Listening

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import { Application } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+
+import { MONGO_URL } from "./constants/pokeApi.constants";
+
+class App 
+{
+    public app: Application;
+
+    constructor() 
+    {
+        this.app = express();
+        this.setConfig();
+        this.setMongoConfig();
+    }
+
+    private setConfig() 
+    {
+        // Allows us to receive requests with data in json format
+        this.app.use(bodyParser.json({ limit: "50mb" }));
+        // Allows us to receive requests with data in x-www-form-urlencoded format
+        this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+        // Enables cors
+        this.app.use(cors());
+    }
+
+    private setMongoConfig() 
+    {
+        mongoose.Promise = global.Promise;
+        
+        mongoose.connect(MONGO_URL);
+    }
+
+}
+
+export default new App().app;
---
+++ src/constants/pokeApi.constants.ts
@@ -0,0 +1,3 @@
+export const PORT = 9001;
+
+export const MONGO_URL = "mongodb://localhost:27017/Pokemons";
